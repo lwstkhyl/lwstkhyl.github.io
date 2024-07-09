fadb5e4b4503de8b17924f2841b149c40354c396
lwstkhyl <1271943237@qq.com>
2024-07-09 18:27:42 +0800

    update scrollspy

--- assets/scripts/scrollspy.js
+++ assets/scripts/scrollspy.js
@@ -3,14 +3,6 @@ $(document).ready(function () {
     const h_list = $('h3,h4,h5'); //所有的标题
     const a_list = $('.toc a'); //所有的目录a标签
     a_list.prop('href', '#'); //禁用a标签锚点跳转
-    // let h_distance = []; //各标题距页面顶端距离
-    // let a_distance = []; //各目录a标签距目录div顶端距离
-    // h_list.each(function (index, dom) {
-    //     h_distance.push($(dom).offset().top);
-    // });
-    // a_list.each(function (index, dom) {
-    //     a_distance.push($(dom).position().top);
-    // });
     let is_click = false; //添加判断条件--是否是因为点击了按钮而滚动
     a_list.on("click", function () {
         is_click = true; //点击了按钮，进行上锁的操作
@@ -23,6 +15,7 @@ $(document).ready(function () {
         });
         a_list.removeClass("current");
         $(this).addClass("current");
+        update_div_scroll_distance(a_index, is_scroll.prop('checked'));
     });
     //回到顶部按钮
     const toc = $('.toc'); //目录标签
@@ -44,7 +37,26 @@ $(document).ready(function () {
         }, function () { //animate回调函数，当动画播放完执行
             is_click = false; //滚动完了就把锁打开
         });
+        if (!is_scroll.prop('checked')) return;
+        toc.stop().animate({
+            scrollTop: 0
+        });
     });
+    //目录是否随内容滚动按钮
+    let is_scroll = $('<div class="scrollspy-is-scroll"><input id="scrollspy-is-scroll" type="checkbox" checked /><label for="scrollspy-is-scroll">目录随内容滚动</label></div>'); //制作是否跟随按钮
+    toc.after(is_scroll); //添加
+    is_scroll = $('#scrollspy-is-scroll');
+    //根据页面滚动距离更改目录div标签滚动距离
+    function update_div_scroll_distance(i, is_update = true) {
+        if (!is_update) return;
+        const top_distance = a_list.eq(i).position().top - a_list.eq(0).position().top; //指定a标签距顶端距离
+        const improve_distance = parseInt(a_list.eq(0).css('height')) + parseInt(a_list.eq(i).css('height')) * 0; //修正值，让标签更加居中
+        let scroll_height = top_distance - parseInt(toc.css('height')) / 2 + improve_distance; //应滚动的距离（让指定a标签居中）
+        if (scroll_height <= 0) scroll_height = 0;
+        toc.stop().animate({
+            scrollTop: scroll_height
+        });
+    }
     //根据页面滚动距离更改目录a标签
     function update_classname() { //根据页面滚动距离更改目录a标签
         const scroll_distance = $(document).scrollTop(); //当前页面滚动的距离
@@ -52,6 +64,7 @@ $(document).ready(function () {
             if (scroll_distance + 1 >= h_list.eq(i).offset().top) { //找到滚动到了哪个h标签
                 a_list.removeClass("current");
                 a_list.eq(i).addClass('current'); //让对应的a标签改变
+                update_div_scroll_distance(i, is_scroll.prop('checked'));
                 break;
             }
         }

d6b33137bd09f2a49839c3b0971141ced005e62f
lwstkhyl <1271943237@qq.com>
2024-07-24 17:44:56 +0800

    导航栏所在页面标签变灰

---
+++ assets/scripts/site.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const li = $("header.header nav.nav--header ul.list--nav li.item--nav");
+    const title = $("main.main small.post-meta span.label--category a").text();
+    li.each(function (index, dom) {
+        const href_list = $(dom).children("a").prop("href").split('/');
+        const a_title1 = href_list[href_list.length - 2];
+        const a_title2 = $(dom).children("a").text();
+        if (a_title1 == title || a_title2 == title) {
+            $(dom).addClass("current").siblings().removeClass("current");
+            return;
+        }
+    })
+    $("nav ul.sub_nav li.sub_nav a.sub_nav").each(function (index, dom) {
+        const href_list = $(dom).prop("href").split('/');
+        const a_title1 = href_list[href_list.length - 2];
+        const a_title2 = $(dom).text();
+        if (a_title1 == title || a_title2 == title) {
+            $(dom).addClass("current").siblings().removeClass("current");
+            $(dom).parent("li.sub_nav").parent("ul.sub_nav").parent("li.item--nav").children("a.no-hover").addClass("current").siblings().removeClass("current");
+            return;
+        }
+    });
+}); 

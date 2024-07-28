14539c232413599833e69c85aa322635dc077f76
lwstkhyl <1271943237@qq.com>
2024-07-28 12:25:24 +0800

    页面加载完成事件

--- assets/scripts/spinner.js
+++ assets/scripts/spinner.js
@@ -1,7 +1,7 @@
 (function () {
     const cover = document.querySelector(".loader-bg"); //遮罩层
     const loader = document.querySelector(".banter-loader"); //动画
-    $(window).on("load", function () { //加载完成事件
+    window.onload = function () { //加载完成事件
         if (typeof jQuery === 'undefined') { //如果jQuery未加载，
             cover.style.display = "none"; //就使用原生js方式隐藏遮罩层
         }
@@ -9,5 +9,5 @@
             $(cover).fadeOut("normal", "swing", () => cover.style.display = "none"); //淡出方式隐藏遮罩层
         }
         loader.style.display = "none"; //隐藏动画
-    });
+    };
 })();

2c6f0e9de0cddbde97b0c403ed3fb65c172fdc2e
lwstkhyl <1271943237@qq.com>
2024-07-28 11:39:44 +0800

    update subcategory

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             $(dom).addClass("current").siblings().removeClass("current");
             return;
         }
-    })
+    });
     $("nav ul.sub_nav li.sub_nav a.sub_nav").each(function (index, dom) {
         const href_list = $(dom).prop("href").split('/');
         const a_title1 = href_list[href_list.length - 2];
@@ -17,6 +17,17 @@ $(document).ready(function () {
         if (a_title1 == title || a_title2 == title) {
             $(dom).addClass("current").siblings().removeClass("current");
             $(dom).parent("li.sub_nav").parent("ul.sub_nav").parent("li.item--nav").children("a.no-hover").addClass("current").siblings().removeClass("current");
+            return;
+        }
+    });
+    li.each(function (index, dom) {
+        const href_list = $(dom).children("a").prop("href").split('/');
+        const a_title = href_list[href_list.length - 2];
+        const url_list = location.href.split('/');
+        const title = url_list[url_list.length - 2];
+        if (a_title == title) {
+            $(dom).addClass("current").siblings().removeClass("current");
+            $(dom).find("a.sub_nav").css("color", "#05bf85");
             return;
         }
     });

95cdec3dbcd2f175271e9bd109e3bd610c0497ab
lwstkhyl <1271943237@qq.com>
2024-08-16 21:39:00 +0800

    update confirm_href.js

--- assets/scripts/confirm_href.js
+++ assets/scripts/confirm_href.js
@@ -6,5 +6,10 @@ $(document).ready(function () {
             href = href.slice(1);
             a.prop("href", href);
         }
+        //如果是手机版且有子标题，点击主标题不跳转，而是打开子标题ul
+        if ($("button.button--nav").css("display") !== 'none'
+            && a.children("span.icon-down-line-free").length != 0) {
+            a.prop("href", "javascript:;");
+        }
     });
 });

49c892adf9a983adc8ccca0469fcfae705059384
lwstkhyl <1271943237@qq.com>
2024-09-22 10:54:51 +0800

    change js/css文件引入路径

--- assets/scripts/img_click.js
+++ assets/scripts/img_click.js
@@ -1,13 +1,15 @@
-const imgs = $("article.article img");
-const modal = $(".modal, .modal-image, .modal-bg");
-const modal_img = $(".modal-image");
-imgs.each((index, img) => {
-    if ($(img).parent().prop("class") === "scrollspy-backtotop") return;
-    $(img).on("click", () => {
-        modal.fadeIn("fast");
-        modal_img.prop("src", $(img).prop("src"));
+$(document).ready(function () {
+    const imgs = $("article.article img");
+    const modal = $(".modal, .modal-image, .modal-bg");
+    const modal_img = $(".modal-image");
+    imgs.each((index, img) => {
+        if ($(img).parent().prop("class") === "scrollspy-backtotop") return;
+        $(img).on("click", () => {
+            modal.fadeIn("fast");
+            modal_img.prop("src", $(img).prop("src"));
+        });
     });
-});
-modal.on("click", () => {
-    modal.fadeOut("fast");
+    modal.on("click", () => {
+        modal.fadeOut("fast");
+    });
 });

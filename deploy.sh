b5998616b4869f036002bbd1e53644368b3e66be
lwstkhyl <12719432237@qq.com>
2024-11-22 13:53:00 +0800

    init 2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 jekyll b
+echo "_site update success"
 sudo rm -rf /var/www/html/*
 sudo cp -r ./_site/* /var/www/html
-echo "success"
+echo "file copy-paste success"
+sudo systemctl restart apache2
+echo "apache2 restart success"
 

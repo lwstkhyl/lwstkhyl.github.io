040b0c02bab7a0f06d08808795e85bd0cbf3c708
lwstkhyl <1271943237@qq.com>
2024-11-22 23:05:05 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
-PATH="$PATH:/home/wth/gems/bin"
-# source ~/.bash_profile
-# source ~/.bashrc
-# source /etc/profile
-# export JEKYLL_HOME=/home/wth/gems
-# export PATH=$PATH:$JEKYLL_HOME/bin
-cd /home/wth/Desktop/lwstkhyl.github.io
-if [ "$(git pull)" = "Already up to date." ]; then
-    exit
-fi
-time=$(date "+%Y-%m-%d %H:%M:%S")
-echo "${time}-----update" >> ./deploy.log
-jekyll b
-if [ $? -eq 0 ]; then
-    echo "_site update success" >> ./deploy.log
+if [ -z $1 ];then
+    time=60
 else
-    echo "_site update failed" >> ./deploy.log
+    time=$1
 fi
+while true
+do 
+    sleep $time
+    cd /home/wth/Desktop/lwstkhyl.github.io
+    if [ "$(git pull)" = "Already up to date." ]; then
+        exit
+    fi
+    date_time=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "${date_time}-----update" >> ./deploy.log
+    jekyll b
+    if [ $? -eq 0 ]; then
+        echo "_site update success" >> ./deploy.log
+    else
+        echo "_site update failed" >> ./deploy.log
+    fi
+done
 # sudo rm -rf /var/www/html/*
 # sudo cp -r ./_site/* /var/www/html
 # echo "file copy-paste success" >> ./deploy.log

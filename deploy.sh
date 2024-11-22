b5862c8659866ce19fff2f58e297cc951f1a648e
lwstkhyl <1271943237@qq.com>
2024-11-22 22:33:17 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 cd /home/wth/Desktop/lwstkhyl.github.io
 if [ "$(git pull)" = "Already up to date." ]; then
     exit
 fi
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${time}-----update" >> ./deploy.log
-/home/wth/gems/bin/jekyll b
-echo "_site update success" >> ./deploy.log
+echo "$(/home/wth/gems/bin/jekyll b)" >> ./deploy.log
+# if [ $? -eq 0 ]; then
+#     echo "_site update success" >> ./deploy.log
+# else
+#     echo "_site update failed" >> ./deploy.log
+# fi
 # sudo rm -rf /var/www/html/*
 # sudo cp -r ./_site/* /var/www/html
 # echo "file copy-paste success" >> ./deploy.log

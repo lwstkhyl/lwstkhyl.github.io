4f782e7a12309bf6bd7233725081f5d81726a2c1
lwstkhyl <12719432237@qq.com>
2024-11-22 18:23:26 +0800

    server

--- autosync.sh
+++ autosync.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-git status
-git add *
-git commit -m "server code change"
 git pull --rebase server
-git push server
-git stash pop
-echo "code sync success"
+echo "code sync success" >> ./deploy.log
 sh ./deploy.sh
 

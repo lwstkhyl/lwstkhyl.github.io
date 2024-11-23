4aca8fa544b147080fdb7e82081c628d2782316b
lwstkhyl <1271943237@qq.com>
2024-11-23 11:58:47 +0800

    111

--- new_deploy.sh
+++ new_deploy.sh
@@ -4,14 +4,6 @@ pull_res=$(git pull)
 if [ "$pull_res" != "Already up to date." ]; then
     date_time=$(date "+%Y-%m-%d %H:%M:%S")
     echo "${date_time}-----update" >> ./deploy.log
-    if [[ "$pull_res" =~ "package.json" ]]; then
-        npm update
-        if [ $? -eq 0 ]; then
-            echo "NodeJS package update success" >> ./deploy.log
-        else
-            echo "NodeJS package update failed" >> ./deploy.log
-        fi
-    fi
     jekyll b
     if [ $? -eq 0 ]; then
         echo "_site update success" >> ./deploy.log

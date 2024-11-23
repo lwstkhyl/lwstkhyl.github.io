f6304ba01ecdc1dae749c300bd74a4eab0049d7b
lwstkhyl <1271943237@qq.com>
2024-11-23 09:08:05 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 function deploy(){
     cd /home/wth/Desktop/lwstkhyl.github.io
-    if [ "$(git pull)" != "Already up to date." ]; then
+    pull_res=$(git pull)
+    if [ "$pull_res" != "Already up to date." ]; then
         date_time=$(date "+%Y-%m-%d %H:%M:%S")
         echo "${date_time}-----update" >> ./deploy.log
+        if [[ "$pull_res" =~ "package.json" ]]; then
+            npm update
+            if [ $? -eq 0 ]; then
+                echo "NodeJS package update success" >> ./deploy.log
+            else
+                echo "NodeJS package update failed" >> ./deploy.log
+            fi
+        fi
         jekyll b
         if [ $? -eq 0 ]; then
             echo "_site update success" >> ./deploy.log

3e03acfb67f5d99ef86883660c0b48b5233859d8
lwstkhyl <1271943237@qq.com>
2025-02-02 02:14:53 +0800

    0201

--- assets/scripts/change_a.js
+++ assets/scripts/change_a.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
         if ($(dom).prop("class") === "title") return;  //如果是title类a标签（笔记入口）直接返回
         const href = $(dom).prop("href");
         if (href === "javascript:;" || href === "#") return;  //如果是无href标签直接返回
-        if ((!href.includes("lwstkhyl.cn") && !href.includes("lwstkhyl.xyz") && !href.includes("127.0.0.1:4000"))  //如果是站外链接，就在新页面中打开
-            || ((href.includes("lwstkhyl.cn") || href.includes("lwstkhyl.xyz") || href.includes("127.0.0.1:4000")) && href.includes("upload"))) { //如果是站内的upload链接，也在新页面中打开
+        if ((!href.includes("lwstkhyl.cn") && !href.includes("lwstkhyl.github.io") && !href.includes("127.0.0.1:4000"))  //如果是站外链接，就在新页面中打开
+            || ((href.includes("lwstkhyl.cn") || href.includes("lwstkhyl.github.io") || href.includes("127.0.0.1:4000")) && href.includes("upload"))) { //如果是站内的upload链接，也在新页面中打开
             $(dom).prop("target", "_blank");
         }
         else if (href.includes("#")) { //如果是站内锚点链接，修改其点击事件为双击了对应标题进行跳转
